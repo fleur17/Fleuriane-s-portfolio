d062887d4b283fd034c9c34b337bad19cda25d5b
LyKell <sv-kevin@hotmail.fr>
2026-04-10 18:54:29 +0200

    feat: add AI Fashion project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,112 +1,7 @@
-import { AppProject, ProjectShowcaseData } from "@/types";
-
-export const projectsShowcases: ProjectShowcaseData[] = [
-  {
-    textSection: {
-      projectType: "ART PROJECT",
-      name: "The NeuroPortals",
-      description: "What if you can be the master of your brain through art ?",
-      href: "/neuroportal",
-      image: {
-        src: "/images/common/touchdesigner.svg",
-        width: 150,
-        height: 50,
-        alt: "Touchdesigner",
-      },
-      position: "left",
-    },
-    imageSection: [
-      {
-        src: "/images/projects/neuroportal/neuro.svg",
-        alt: "Neuro background",
-        width: 800,
-        height: 800,
-        className: "group-hover:hidden",
-      },
-      {
-        src: "/images/projects/neuroportal/hover-neuro-home.svg",
-        alt: "Neuro background hover",
-        width: 800,
-        height: 800,
-        className: "hidden group-hover:block",
-      },
-    ],
-  },
-  {
-    textSection: {
-      projectType: "BUSINESS PROJECT",
-      name: "The Elastup",
-      description: "For your notebook, Tupperware and more ...",
-      href: "/elastup",
-      image: {
-        src: "/images/common/kickstarter-logo.svg",
-        width: 200,
-        height: 100,
-        alt: "Kickstarter",
-      },
-      position: "right",
-    },
-    imageSection: [
-      {
-        src: "/images/projects/elastup/elastup-group.svg",
-        alt: "Elastup",
-        width: 700,
-        height: 700,
-      },
-    ],
-  },
-  {
-    textSection: {
-      projectType: "UX/UI PROJECT",
-      name: "NewFaces",
-      description: "The App that will teach you all about fashion!",
-      href: "/newfaces",
-      image: {
-        src: "/images/common/figma-logo.svg",
-        width: 150,
-        height: 50,
-        alt: "Figma",
-      },
-      position: "left",
-    },
-    imageSection: [
-      {
-        src: "/images/projects/newfaces/newfaces.png",
-        alt: "New Faces background",
-        width: 800,
-        height: 800,
-      },
-    ],
-  },
-  {
-    textSection: {
-      projectType: "UX/UI PROJECT",
-      name: "AI Fashion",
-      description: "Designing an AI-powered fashion ecosystem",
-      href: "/ai-fashion",
-      image: {
-        src: "/images/common/figma-logo.svg",
-        width: 150,
-        height: 50,
-        alt: "Figma",
-      },
-      position: "right",
-    },
-    imageSection: [
-      {
-        src: "/images/projects/ai-fashion/ai-fashion.svg",
-        alt: "AI Fashion",
-        width: 800,
-        height: 800,
-      },
-    ],
-  },
-];
+import { AppProject } from "@/types";
 
 export const projects: AppProject[] = [
   {
-    slug: "newfaces",
-    layout: "app-project",
     title: "NewFaces — Your Fashion Learning Platform",
     description:
       "I designed NewFace a fictionnal user-centric platform to streamline talent discovery for actors, models, and agencies, enhancing profile visibility and engagement through intuitive UX/UI design.",
@@ -210,5 +105,107 @@ export const projects: AppProject[] = [
     },
     nextSteps:
       "Conduct usability testing with real users, refine micro-interactions, and collaborate with developers to ensure design fidelity during implementation.",
+    slug: "newfaces",
+    layout: "app-project",
+  },
+  {
+    title: "AI Fashion Intelligent Styling Experience",
+    description:
+      "I designed a fictionnal UI/UX IA fashion app that helps people find style recommendations being able to virtually try on clothes and get personalized outfit ideas using artificial intelligence.",
+    carousel: [
+      {
+        src: "/images/projects/ai-fashion/home.svg",
+      },
+      {
+        src: "/images/projects/ai-fashion/calendar.svg",
+      },
+      {
+        src: "/images/projects/ai-fashion/chat.svg",
+      },
+    ],
+    researchInsights: {
+      research:
+        "Through research including surveys and interviews with fashion shoppers, i manage to find some relevant points that could be resolved through my app.",
+      insights: [
+        "Users want personalized outfit suggestions.",
+        "Fashion professionals struggle to keep their schedules updated.",
+        "Retailers need smarter ways to showcase products with AI.",
+      ],
+      image: {
+        src: "/images/common/desk.jpg",
+        width: 200,
+        height: 200,
+        alt: "AI Fashion research",
+      },
+    },
+    userPersonas: {
+      description:
+        "Those are fictionnal personas that helped me define user needs and refine AI-powered features.",
+      personas: [
+        {
+          name: "Mia, Trend-Seeker",
+          age: 22,
+          goals: "Discover new outfits instantly using AI",
+          painPoints: "Hard to match clothes with her style",
+          image: {
+            src: "/images/common/woman.png",
+          },
+        },
+        {
+          name: "Daniel, Profesional model",
+          age: 30,
+          goals: "Being more informed about the fashion world",
+          painPoints: "Not enough time to stay properly informed",
+          image: {
+            src: "/images/common/man.png",
+          },
+        },
+        {
+          name: "Retail Fashion Manager",
+          age: 38,
+          goals:
+            "Improve product visibility and boost customer engagement with AI styling",
+          painPoints: "Low visibility, low engagement, and high returns",
+          image: {
+            src: "/images/common/woman.png",
+          },
+        },
+      ],
+    },
+    lowFidelityPrototyping: {
+      description: "Sketches and wireframes mapping out AI-driven user flows",
+      images: [
+        {
+          src: "/images/projects/ai-fashion/home_proto.svg",
+          alt: "Wireframe 1",
+        },
+        {
+          src: "/images/projects/ai-fashion/calendar-proto.svg",
+          alt: "Wireframe 2",
+        },
+      ],
+    },
+    highFidelityPrototyping: {
+      description:
+        "Prototypes showcasing AI features and the different pages of the app",
+      images: [
+        {
+          src: "/images/projects/ai-fashion/home.svg",
+          alt: "Home page",
+        },
+        {
+          src: "/images/projects/ai-fashion/calendar.svg",
+          alt: "Calendar page",
+        },
+        {
+          src: "/images/projects/ai-fashion/chat.svg",
+          alt: "Chatbot page",
+        },
+      ],
+    },
+    nextSteps:
+      "Further user testing to refine AI recommendations and to improve the user flow of the app",
+    slug: "ai-fashion",
+    layout: "app-project",
   },
 ];
