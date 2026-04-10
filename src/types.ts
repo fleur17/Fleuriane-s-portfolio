c4130e7a4853e59ec317641d368e2f2bd579d768
LyKell <sv-kevin@hotmail.fr>
2026-04-10 18:14:30 +0200

    feat: add AppProject types

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
 export type ImageType = {
   src: string;
-  width: number;
-  height: number;
-  alt: string;
+  width?: number;
+  height?: number;
+  alt?: string;
   className?: string;
 };
 
-export interface ProjectData {
+export interface ProjectShowcaseData {
   textSection: {
     projectType: string;
     name: string;
@@ -22,3 +22,39 @@ export type NavLink = {
   href: string;
   label: string;
 };
+
+export type Prototyping = {
+  description: string;
+  images: ImageType[];
+};
+
+export type LayoutKey = "app-project";
+
+export type Layout = {
+  slug: string;
+  layout: LayoutKey;
+};
+
+export interface AppProject extends Layout {
+  title: string;
+  description: string;
+  carousel: ImageType[];
+  researchInsights: {
+    research: string;
+    insights: string[];
+    image: ImageType;
+  };
+  userPersonas: {
+    description: string;
+    personas: {
+      name: string;
+      age: number;
+      goals: string;
+      painPoints: string;
+      image: ImageType;
+    }[];
+  };
+  lowFidelityPrototyping: Prototyping;
+  highFidelityPrototyping: Prototyping;
+  nextSteps: string;
+}
