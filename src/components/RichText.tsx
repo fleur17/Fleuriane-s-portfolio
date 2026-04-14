9fefefe8c827610f591e2970b2a650525366087c
LyKell <sv-kevin@hotmail.fr>
2026-04-14 23:07:09 +0200

    feat: update richtext styling

--- src/components/RichText.tsx
+++ src/components/RichText.tsx
@@ -8,28 +8,32 @@ interface MarkdownTextProps {
 
 export default function RichText({ text, className }: MarkdownTextProps) {
   return (
-    <Markdown
-      rehypePlugins={[rehypeRaw]}
-      components={{
-        p: ({ children }) => <p className={className}>{children}</p>,
-        a: ({ href, children }) => (
-          <a
-            href={href}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline hover:opacity-70"
-          >
-            {children}
-          </a>
-        ),
-        strong: ({ children }) => (
-          <strong className="font-bold">{children}</strong>
-        ),
-        ul: ({ children }) => <ul className="list-disc pl-5">{children}</ul>,
-        li: ({ children }) => <li className="mt-1">{children}</li>,
-      }}
-    >
-      {text}
-    </Markdown>
+    <div>
+      <Markdown
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          p: ({ children }) => <p className={className}>{children}</p>,
+          a: ({ href, children }) => (
+            <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline hover:opacity-70"
+            >
+              {children}
+            </a>
+          ),
+          strong: ({ children }) => (
+            <strong className="font-bold">{children}</strong>
+          ),
+          ul: ({ children }) => <ul className="list-disc pl-5">{children}</ul>,
+          li: ({ children }) => (
+            <li className="mb-3 text-[18px]">{children}</li>
+          ),
+        }}
+      >
+        {text}
+      </Markdown>
+    </div>
   );
 }
