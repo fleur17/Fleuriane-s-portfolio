34c093092f7cdea43529e080c6759baf73a1e7e7
LyKell <sv-kevin@hotmail.fr>
2026-04-19 03:30:23 +0200

    feat: clean ui projects

---
+++ src/data/ui-projects.ts
@@ -0,0 +1,22 @@
+export const uiProjects = [
+  {
+    src: "/images/projects/ui-projects/timer-project.svg",
+    label: "Timer Project - UI Exploration",
+  },
+  {
+    src: "/images/projects/ui-projects/iphone-interface.svg",
+    label: "iPhone Interface - Visual Concept",
+  },
+  {
+    src: "/images/projects/ui-projects/nadz-model.svg",
+    label: "Fashion social media - App UI",
+  },
+  {
+    src: "/images/projects/ui-projects/spotify-redesign.svg",
+    label: "Spotify Redesign - UI Concept",
+  },
+  {
+    src: "/images/projects/ui-projects/be-real-emotions.svg",
+    label: "Emotion Capture - UI Design",
+  },
+];
